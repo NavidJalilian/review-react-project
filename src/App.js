c257ccfb6a6ecd6b6c542531c9f904cdf4a7e98b
Navidj1360 <95021931+Navidj1360@users.noreply.github.com>
2022-03-31 12:08:09 -0700

    random btn + prev-next-btn done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
-import Review from './Review';
+import React, { useState } from "react";
+import Review from "./Review";
+
+import {} from "react-icons/fa";
+import people from "./data";
 function App() {
-  return <h2>reviews project setup</h2>;
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>our reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Review></Review>
+        {/* a function that adds two numbers */}
+      </section>
+    </main>
+  );
 }
 
 export default App;
